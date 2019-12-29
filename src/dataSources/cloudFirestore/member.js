d645f6a860991d31b1e267a43da73ce64556d09a
Clark Sell <clark.sell@csell.net>
2019-12-29 17:08:21 -0600

    adds create and update member

--- src/dataSources/cloudFirestore/member.js
+++ src/dataSources/cloudFirestore/member.js
@@ -1,14 +1,64 @@
+import debug from 'debug';
+
+const dlog = debug('that:api:members:datasources:members');
+
+function scrubProfile(profile, isNew) {
+  const scrubbedProfile = profile;
+
+  const modifiedAtDate = new Date().toISOString();
+
+  if (isNew) {
+    scrubbedProfile.createdAt = modifiedAtDate;
+  }
+
+  scrubbedProfile.lastUpdatedAt = modifiedAtDate;
+
+  return scrubbedProfile;
+}
+
 const event = (dbInstance, logger) => {
   const collectionName = 'members';
   const membersCol = dbInstance.collection(collectionName);
 
-  const create = async newSession => {};
+  async function create({ user, profile }) {
+    const docRef = membersCol.doc(user.sub);
 
-  const find = async id => {};
+    const modifiedProfile = scrubProfile(profile, true);
 
-  const update = (id, eventInput) => {};
+    await docRef.set(modifiedProfile, { merge: true });
+    const updatedDoc = await docRef.get();
 
-  return { create, find, update };
+    return {
+      id: docRef.id,
+      ...updatedDoc.data(),
+    };
+  }
+
+  async function findMe(memberId) {
+    const docRef = dbInstance.doc(`${collectionName}/${memberId}`);
+    const doc = await await docRef.get();
+    return {
+      id: doc.id,
+      ...doc.data(),
+    };
+  }
+
+  async function update({ memberId, profile }) {
+    dlog('db update called');
+
+    const docRef = dbInstance.doc(`${collectionName}/${memberId}`);
+
+    const modifiedProfile = scrubProfile(profile);
+    await docRef.update(modifiedProfile);
+
+    const updatedDoc = await docRef.get();
+    return {
+      id: updatedDoc.id,
+      ...updatedDoc.data(),
+    };
+  }
+
+  return { create, findMe, update };
 };
 
 export default event;
