e64bbb8ab32b8f5c43a2ded1675c3b938cb6e69f
Brett Slaski <brett@thatconference.com>
2020-10-07 20:13:42 -0500

    Add PulbicProfiles sessions filter

--- src/graphql/resolvers/queries/publicProfile.js
+++ src/graphql/resolvers/queries/publicProfile.js
@@ -16,11 +16,45 @@ export const fieldResolvers = {
       return user;
     },
     earnedMeritBadges: meritBadgesResolver.earnedMeritBadges,
-    sessions: ({ id }, __, { dataSources: { firestore } }) => {
-      dlog('sessions for %s', id);
-      return sessionStore(firestore).findMembersAcceptedSessions({
-        memberId: id,
-      });
+    sessions: (
+      { id },
+      { filter, asOfDate },
+      { dataSources: { firestore } },
+    ) => {
+      dlog('sessions for %s with filter %s', id, filter);
+
+      let localFilter = 'UPCOMING';
+      if (filter) localFilter = filter;
+      // today at 00:00:00.000 (in epoch number format)
+      let parsableTime = new Date().setHours(0, 0, 0, 0);
+      if (asOfDate) {
+        // dates from json are string formatted hopefully iso format
+        parsableTime = asOfDate;
+      }
+      const targetDate = new Date(parsableTime);
+      dlog('as of date: %s', targetDate);
+      let result;
+      if (localFilter === 'ALL') {
+        result = sessionStore(firestore).findMembersAcceptedSessions({
+          memberId: id,
+        });
+      } else if (localFilter === 'UPCOMING') {
+        result = sessionStore(firestore).findMembersAcceptedSessionsFromDate({
+          memberId: id,
+          fromDate: targetDate,
+        });
+      } else if (localFilter === 'PAST') {
+        result = sessionStore(firestore).findMembersAcceptedSessionsBeforeDate({
+          memberId: id,
+          beforeDate: targetDate,
+        });
+      } else
+        throw new Error(
+          'Unknown AcceptedSessionFilter value provided. %s',
+          filter,
+        );
+
+      return result;
     },
   },
 };
