54a92c4c09917fa67e4e4782828b63221f2194f9
Brett Slaski <brett@thatconference.com>
2021-10-29 08:25:26 -0500

    oops

---
+++ src/envConfig.js
@@ -0,0 +1,29 @@
+function configMissing(configKey) {
+  throw new Error(`missing required .env setting for ${configKey}`);
+}
+
+const requiredConfig = () => ({
+  postmarkApiToken:
+    process.env.POSTMARK_API_TOKEN || configMissing('POSTMARK_API_TOKEN'),
+  titoCheckinSlug:
+    process.env.TITO_CHECKIN_SLUG || configMissing('TITO_CHECKIN_SLUG'),
+  defaultProfileImage:
+    'https://images.that.tech/members/person-placeholder.jpg',
+  activeCampaignApi:
+    process.env.ACTIVE_CAMPAIGN_API || configMissing('ACTIVE_CAMPAIGN_API'),
+  activeCampaignKey:
+    process.env.ACTIVE_CAMPAIGN_KEY || configMissing('ACTIVE_CAMPAIGN_KEY'),
+  acRegisteredFromField: '20',
+  slack: {
+    webhookUrl:
+      process.env.SLACK_WEBHOOK_URL || configMissing('SLACK_WEBHOOK_URL'),
+    inviteUrl:
+      process.env.SLACK_INVITE_URL ||
+      'https://slack.com/api/users.admin.invite',
+    legacyToken:
+      process.env.SLACK_LEGACY_TOKEN || configMissing('SLACK_LEGACY_TOKEN'),
+    memberNotificationChannel: '#introductions',
+  },
+});
+
+export default requiredConfig();
